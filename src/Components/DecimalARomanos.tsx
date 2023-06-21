ef3c1b4a9c014c516bb4c5d729637f18efbe918f
FacuBritez <FacundoIvanBritez@gmail.com>
2023-06-21 20:18:17 -0300

    Logica agregada, faltan detalles

--- src/Components/DecimalARomanos.tsx
+++ src/Components/DecimalARomanos.tsx
@@ -1,16 +1,66 @@
 import React from "react";
+import { useState } from "react";
 
-function RomanosADecimal() {
+function DecimalARomanos() {
+
+    const [decimal, setDecimal] = useState(0);
+    const [romanResult, setRomanResult] = useState('');
+
+    const convertToRoman = (num: number) => {
+        let numeroRomano = "";
+        let numerosRomanos: [string, number][] = [
+            ["M", 1000],
+            ["CM", 900],
+            ["D", 500],
+            ["CD", 400],
+            ["C", 100],
+            ["XC", 90],
+            ["L", 50],
+            ["XL", 40],
+            ["X", 10],
+            ["IX", 9],
+            ["V", 5],
+            ["IV", 4],
+            ["I", 1]
+        ];
+
+        //Limitando el rango del imput
+        if (num < 1) {
+            console.log("El numero no puede ser menor a 1")
+            return ""
+        }
+        if (num > 3999) {
+            console.log("El numero no puede ser mayor a 3999")
+            return ""
+        }
+
+        //Itera sobre los array de numeros romanos y sus valores en decimal [string,number]
+        for (let i = 0; i < numerosRomanos.length; i++) {
+            for (let j = 3; j > 0; j--) {
+                if (num >= numerosRomanos[i][1]) {
+                    numeroRomano += numerosRomanos[i][0];
+                    num -= numerosRomanos[i][1];
+                }
+            }
+        }
+
+        console.log(numeroRomano)
+        setRomanResult(numeroRomano)
+        setDecimal(0)
+    }
+
+
+
     return (
         <div className="conversor text-center">
             <p>Pasar número decimal a numero romano</p>
-            <input id="numeroDecimal" placeholder="Numero decimal" />
+            <input id="numeroDecimal" placeholder="Numero decimal" defaultValue={decimal} onChange={(e) => setDecimal(Number(e.target.value))} />
             <br />
-                <button onClick={() => actualiza(numeroDecimal)}>Convertir a Romano</button>
+            <button onClick={() => convertToRoman(decimal)}>Convertir a Romano</button>
             <br />
-                <input id="romanResult" placeholder="Numero romano" readOnly />
+            <input id="romanResult" placeholder="Numero romano" defaultValue={romanResult} readOnly />
         </div>
     );
 }
 
-export default RomanosADecimal;
+export default DecimalARomanos;
--- src/Components/RomanosADecimal.tsx
+++ src/Components/RomanosADecimal.tsx
@@ -1,14 +1,100 @@
-import React from "react"
+import React from "react";
+import { useState } from "react";
 
 function RomanosADecimal() {
+
+    const [romano, setRomano] = useState('');
+    const [resultDecimal, setResultDecimal] = useState('');
+
+    const convertToDecimal = (num: string) => {
+
+        //Para que acepte minusculas
+        num = num.toUpperCase()
+
+        //Restricciones
+        //      MILES      CENTENAS        DECENAS         UNIDADES
+        if (!/^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(num)) {
+            return ''
+        }
+
+        let numeroDecimal = 0;
+        let numeroAnterior: number | null = null;
+        let numeroActual: number;
+
+        let numerosRomanos: [string, number][] = [
+            ["M", 1000],
+            ["D", 500],
+            ["C", 100],
+            ["L", 50],
+            ["X", 10],
+            ["V", 5],
+            ["I", 1]
+        ];
+
+        //recorre el string num
+        for (let i = 0; i < num.length; i++) {
+
+            //recorre el array de numeros romanos
+            for (let j = 0; j < numerosRomanos.length; j++) {
+
+                //Si la letra coincide
+                if (num[i] === numerosRomanos[j][0]) {
+
+                    //Cambiamos la variable del numero actual para mayor legibilidad
+                    numeroActual = numerosRomanos[j][1];
+
+
+                    //Si la letra es la primera del numero romano
+                    if (numeroAnterior == null) {
+
+                        //actualizamos la variable numeroAnterior
+                        numeroAnterior = numeroActual;
+                    }
+
+
+                    //Si la letra esta siendo restada por su letra anterior
+                    if (numeroAnterior < numeroActual) {
+
+                        //en la vuelta anterior se sumo, asi que primero la restamos 2 veces al numeroDecimal
+                        numeroDecimal -= numeroAnterior * 2;
+
+                        //despues sumamos el numero actual(la resta se aplico en el paso anterior)
+                        numeroDecimal += numeroActual;
+
+                        // y actualizamos el numeroAnterior
+                        numeroAnterior = numeroActual /* - numeroAnterior  Creo que esto no va, pero para no borrarlo, lo comento*/;
+                    }
+
+
+                    //Si la letra NO esta siendo restada por su letra anterior
+                    else {
+
+                        //Lo sumamos al numeroDecimal
+                        numeroDecimal += numeroActual;
+
+                        //y actualizamos el numeroAnterior
+                        numeroAnterior = numeroActual;
+                    }
+                }
+            }
+        }
+        setResultDecimal(
+            numeroDecimal === 0 ? '' : numeroDecimal.toString()
+        );
+        setRomano('');
+    }
+
+
+
+
     return (
         <div className="conversor text-center">
             <p>Pasar número romano a numero decimal</p>
-            <input id="numeroRomano" placeholder="Numero romano" />
+            <input id="numeroRomano" placeholder="Numero romano" defaultValue={romano} onChange={(e) => setRomano(e.target.value)} />
             <br />
-                <button onClick={() => actualiza(numeroRomano)}>Convertir a decimal</button>
+            <button onClick={() => convertToDecimal(romano)}>Convertir a decimal</button>
             <br />
-                <input id="decimalResult" placeholder="Numero decimal" />
+            <input id="decimalResult" placeholder="Numero decimal" defaultValue={resultDecimal} />
         </div>
     );
 }
